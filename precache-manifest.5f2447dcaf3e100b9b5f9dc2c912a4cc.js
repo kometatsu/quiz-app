b02d3ffd0da62dd47da20cc063995dfef61eeb3c
Tatsuya Yonehiro <kometatsu923@gmail.com>
2020-03-08 10:48:35 +0900

    Updates

---
+++ precache-manifest.5f2447dcaf3e100b9b5f9dc2c912a4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3397baa101791c22933fbfa9612379af",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "ff5a26a9ee214b1cea83",
+    "url": "/quiz-app/static/css/main.1dd6a322.chunk.css"
+  },
+  {
+    "revision": "ab890352670d2844c33b",
+    "url": "/quiz-app/static/js/2.59e36b56.chunk.js"
+  },
+  {
+    "revision": "685e179eab013b665e788e7cfaf87ff5",
+    "url": "/quiz-app/static/js/2.59e36b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5a26a9ee214b1cea83",
+    "url": "/quiz-app/static/js/main.a266866c.chunk.js"
+  },
+  {
+    "revision": "574e3a07b0a6264442f0",
+    "url": "/quiz-app/static/js/runtime-main.a7e04a0e.js"
+  }
+]);
